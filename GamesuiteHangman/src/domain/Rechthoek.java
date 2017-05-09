8f23f185df5a0722dc380ee6d7e2c8e5349ba33b
Tiebevn <tiebe.vannieuwenhove@student.ucll.be>
2017-05-09 10:36:26 +0200

    finished Cirkel and Rechthoek

--- GamesuiteHangman/src/domain/Rechthoek.java
+++ GamesuiteHangman/src/domain/Rechthoek.java
@@ -4,8 +4,61 @@ package domain;
  * Created by tiebe on 9/05/2017.
  */
 public class Rechthoek extends Vorm{
+
+
+    private Punt linkerBovenhoek;
+    private int breedte, hoogte;
+
+    public Rechthoek(Punt linkerBovenhoek, int breedte, int hoogte) {
+        this.setBreedte(breedte);
+        this.setHoogte(hoogte);
+        this.setLinkerBovenhoek(linkerBovenhoek);
+    }
+
+    public Punt getLinkerBovenhoek() {
+        return linkerBovenhoek;
+    }
+
+    public void setLinkerBovenhoek(Punt linkerBovenhoek) {
+        if (linkerBovenhoek == null) throw new DomainException("Punt mag niet null zijn");
+        this.linkerBovenhoek = linkerBovenhoek;
+    }
+
+    public int getBreedte() {
+        return breedte;
+    }
+
+    public void setBreedte(int breedte) {
+        if (breedte < 0) throw new DomainException("Breedte mag niet kleiner dan 0 zijn");
+        this.breedte = breedte;
+    }
+
+    public int getHoogte() {
+        return hoogte;
+    }
+
+    public void setHoogte(int hoogte) {
+        if (hoogte < 0) throw new DomainException("Hoogte mag niet kleiner zijn dan 0");
+        this.hoogte = hoogte;
+    }
+
     @Override
     public boolean equals(Object o) {
+
+        if (o == null) return false;
+        if (o instanceof Rechthoek) {
+
+            Rechthoek rechthoek = (Rechthoek)o;
+
+            return  (rechthoek.getLinkerBovenhoek().equals(this.getLinkerBovenhoek()) && rechthoek.getBreedte() == this.getBreedte() && rechthoek.getHoogte() == this.getHoogte());
+
+        }
+
         return false;
+    }
+
+    @Override
+    public String toString() {
+    return "Rechthoek: positie: " + this.getLinkerBovenhoek().toString() + " - breedte: " + this.getBreedte() + " - hoogte: " + this.getHoogte();
     }
 }
--- GamesuiteHangman/src/domain/Vorm.java
+++ GamesuiteHangman/src/domain/Vorm.java
@@ -6,5 +6,6 @@ package domain;
 public abstract class Vorm {
 
     public abstract boolean equals(Object o);
+    public abstract String toString();
 
 }
