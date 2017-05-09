9e1030bb77ffeaecf7e30946d421559a50ba2e2a
Tiebevn <tiebe.vannieuwenhove@student.ucll.be>
2017-05-09 10:39:56 +0200

    Started LijnStuk denk: Sam doe: Tiebe

--- GamesuiteHangman/src/domain/LijnStuk.java
+++ GamesuiteHangman/src/domain/LijnStuk.java
@@ -4,8 +4,37 @@ package domain;
  * Created by tiebe on 9/05/2017.
  */
 public class LijnStuk extends Vorm {
+
+    private Punt startPunt, eindPunt;
+
+    public LijnStuk(Punt startPunt, Punt eindPunt) {
+        this.setEindPunt(eindPunt);
+        this.setStartPunt(startPunt);
+    }
+
+    public Punt getStartPunt() {
+        return startPunt;
+    }
+
+    public void setStartPunt(Punt startPunt) {
+        this.startPunt = startPunt;
+    }
+
+    public Punt getEindPunt() {
+        return eindPunt;
+    }
+
+    public void setEindPunt(Punt eindPunt) {
+        this.eindPunt = eindPunt;
+    }
+
     @Override
     public boolean equals(Object o) {
         return false;
+    }
+
+    @Override
+    public String toString() {
+        return "";
     }
 }
