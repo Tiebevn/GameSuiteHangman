35f599bc049d886fa011f422276950cf9387c350
Aaron <aaronniv@gmail.com>
2017-05-09 10:38:47 +0200

    Story 2 Punt, Doener Aaron, Denker Iben Equals nog niet af.

--- GamesuiteHangman/src/domain/Punt.java
+++ GamesuiteHangman/src/domain/Punt.java
@@ -4,4 +4,30 @@ package domain;
  * Created by tiebe on 9/05/2017.
  */
 public class Punt {
+	private int x;
+	private int y;
+	public int getX() {
+		return x;
+	}
+	public Punt(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	@Override
+	public String toString() {
+		return "("+ x+", "+ y +")";
+	}
+	
+	
+	
 }
--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -11,6 +11,9 @@ public class Launcher {
 		Speler speler = new Speler(naam);
 
 		JOptionPane.showMessageDialog(null, "... zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+		int x =Integer.parseInt(JOptionPane.showInputDialog("X coordinaat van het punt:"));
+		int y =Integer.parseInt(JOptionPane.showInputDialog("Y coordinaat van het punt:"));
+		JOptionPane.showMessageDialog(null, "u heeft een correct punt aangemaakt: ("+ x +", "+ y+")");
 	}
 
 }
