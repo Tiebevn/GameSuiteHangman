f5c41836785bd37180caa5b3a71f4758a92861c2
tiebevn <tiebe.vannieuwenhove@student.ucll.be>
2017-05-09 23:57:07 +0200

    Refactored Launcher

--- GamesuiteHangman/src/ui/Launcher.java
+++ GamesuiteHangman/src/ui/Launcher.java
@@ -1,47 +1,85 @@
 package ui;
 
-import javax.swing.*;
+import domain.*;
 
-import domain.Cirkel;
-import domain.Speler;
-import domain.Punt;
-import domain.PuntDB;
+import javax.swing.*;
 
 public class Launcher {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// VOEG EEN SPELER TOE
-		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
-		Speler speler = new Speler(naam);
+        // VOEG EEN SPELER TOE
+        String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
+        Speler speler = new Speler(naam);
 
-		JOptionPane.showMessageDialog(null, speler.getNaam() + " zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, speler.getNaam() + " zal binnekort spelen", speler.getNaam(), JOptionPane.INFORMATION_MESSAGE);
 
-		Object[] shapes = {"Cirkel", "Rechthoek", "Lijnstuk", "Driehoek"};
-		Object keuze = JOptionPane.showInputDialog(null, "Wat wilt u tekenen?", "input", JOptionPane.INFORMATION_MESSAGE, null, shapes, null);
-
-
-
-		// VOEG EEN PUNT TOE
-		int x =Integer.parseInt(JOptionPane.showInputDialog("X coordinaat van het punt:"));
-		int y =Integer.parseInt(JOptionPane.showInputDialog("Y coordinaat van het punt:"));
-		Punt nieuwPunt = new Punt(x, y);
-
-			if(PuntDB.contains(nieuwPunt)){
-				JOptionPane.showMessageDialog(null, "Dit punt is dubbel");
-			}
-			else{
-				JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: " + nieuwPunt.toString(), speler.getNaam(),JOptionPane.INFORMATION_MESSAGE);
-				PuntDB.voegPuntToe(new Punt(x, y));
-
-			}
+        String[] shapes = {"Cirkel", "Rechthoek", "Lijnstuk", "Driehoek"};
+        String input = (String) JOptionPane.showInputDialog(null, "Kies wat u wilt doen", "Input", JOptionPane.QUESTION_MESSAGE, null, shapes, shapes[0]);
 
 
-		// VOEG EEN STRAAL TOE
+        switch (input) {
+            case "Cirkel":
+                cirkel();
+            case "Rechthoek":
+                rechthoek();
+            case "Lijnstuk":
+                lijnStuk();
+            case "Driehoek":
+                driehoek();
 
-		int radius = Integer.parseInt(JOptionPane.showInputDialog("Radius van de cirkel"));
-		Cirkel cirkel = new Cirkel(nieuwPunt, radius);
-		JOptionPane.showMessageDialog(null, "U heeft een correcte cirkel aangemaakt: " + cirkel.toString());
-	}
+        }
+
+
+    }
+
+    private static Punt punt() {
+
+        Punt punt = new Punt(0, 0);
+
+        punt.setX(Integer.parseInt(JOptionPane.showInputDialog(null, "Geef een X-waarde")));
+        punt.setY(Integer.parseInt(JOptionPane.showInputDialog(null, "Geef een Y-waarde")));
+
+        return punt;
+    }
+
+    private static Cirkel cirkel() {
+
+        Punt middelPunt = punt();
+        Cirkel cirkel = new Cirkel(middelPunt, Integer.parseInt(JOptionPane.showInputDialog(null, "Geef een straal")));
+
+        JOptionPane.showMessageDialog(null, "U heeft een correcte cirkel aangemaakt " + cirkel.toString());
+        return cirkel;
+    }
+
+    private static Rechthoek rechthoek() {
+
+        Punt linkerBovenhoek = punt();
+
+        int breedte = Integer.parseInt(JOptionPane.showInputDialog(null, "Geef een breedte"));
+        int hoogte = Integer.parseInt(JOptionPane.showInputDialog(null, "Geef een hoogte"));
+
+
+        Rechthoek rechthoek = new Rechthoek(linkerBovenhoek, breedte, hoogte);
+
+        JOptionPane.showMessageDialog(null, "U heeft een correcte rechthoek aangemaakt " + rechthoek.toString());
+        return rechthoek;
+    }
+
+    private static Driehoek driehoek() {
+
+        Driehoek driehoek = new Driehoek(punt(), punt(), punt());
+
+        JOptionPane.showMessageDialog(null, "U heeft een correcte rechthoek aangemaakt " + driehoek.toString());
+        return driehoek;
+
+
+    }
+
+    private static LijnStuk lijnStuk() {
+        LijnStuk lijnStuk = new LijnStuk(punt(), punt());
+        JOptionPane.showMessageDialog(null, "U heeft een correcte rechthoek aangemaakt " + lijnStuk.toString());
+        return lijnStuk;
+    }
 
 }
