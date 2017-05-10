41997d4d5e1cdf3f2426b4ad92114a66d1efbb58
Tiebevn <tiebe.vannieuwenhove@student.ucll.be>
2017-05-10 11:47:54 +0200

    Added hangman stub

--- GamesuiteHangman/src/domain/HangMan.java
+++ GamesuiteHangman/src/domain/HangMan.java
@@ -7,5 +7,48 @@ public class HangMan {
 
 
     private Speler speler;
+    private WoordenLijst lijst;
 
+    private boolean gewonnen, gameOver;
+
+    public HangMan(Speler speler, WoordenLijst lijst) {
+        this.setSpeler(speler);
+        this.setLijst(lijst);
+    }
+
+    public boolean raad(char c) {
+        return false;
+    }
+
+    public boolean isGewonnen() {
+        return gewonnen;
+    }
+
+    public void setGewonnen(boolean gewonnen) {
+        this.gewonnen = gewonnen;
+    }
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
+    public void setGameOver(boolean gameOver) {
+        this.gameOver = gameOver;
+    }
+
+    public Speler getSpeler() {
+        return speler;
+    }
+
+    public void setSpeler(Speler speler) {
+        this.speler = speler;
+    }
+
+    public WoordenLijst getLijst() {
+        return lijst;
+    }
+
+    public void setLijst(WoordenLijst lijst) {
+        this.lijst = lijst;
+    }
 }
